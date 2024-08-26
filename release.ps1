4c032d47064d5c6d921a0482b3453bb222246825
laqieer <laqieer@126.com>
2024-08-26 15:28:48 +0800

    Ignore `config/etc` & `config/log` when release

--- release.ps1
+++ release.ps1
@@ -1,8 +1,11 @@
 $folder = "r\"
 if (!(Test-Path $folder)) {
     New-Item -ItemType Directory -Path $folder
+    New-Item -ItemType Directory -Path $folder\config
+    New-Item -ItemType Directory -Path $folder\config\etc
+    New-Item -ItemType Directory -Path $folder\config\log
 }
 Copy-Item -Force "FEBuilderGBA\bin\Release\FEBuilderGBA.exe" $folder
 Copy-Item -Force "FEBuilderGBA\bin\Debug\7-zip32.dll" $folder
-Copy-Item -Force -Recurse "FEBuilderGBA\bin\Debug\config" $folder
+Copy-Item -Force -Recurse "FEBuilderGBA\bin\Debug\config\*" $folder\config -Exclude @("etc","log")
 Copy-Item -Force *.md $folder
