bf4ca618d03ec4f0de144c8ed51a5f1191847ec0
laqieer <laqieer@126.com>
2025-04-11 01:17:46 +0800

    Add missing json files in artifact

--- release.ps1
+++ release.ps1
@@ -6,6 +6,8 @@ if (!(Test-Path $folder)) {
     New-Item -ItemType Directory -Path $folder\config\log
 }
 Copy-Item -Force "FEBuilderGBA\bin\Release\FEBuilderGBA.exe" $folder
-Copy-Item -Force "FEBuilderGBA\bin\Debug\*.dll" $folder
+Copy-Item -Force "FEBuilderGBA\bin\Release\*.dll" $folder
+Copy-Item -Force "FEBuilderGBA\bin\Release\*.json" $folder
+Copy-Item -Force "FEBuilderGBA\bin\Debug\7-zip32.dll" $folder
 Copy-Item -Force -Recurse "FEBuilderGBA\bin\Debug\config\*" $folder\config -Exclude @("etc","log")
 Copy-Item -Force *.md $folder
